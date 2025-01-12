bba36aeaad432c11715dc3a5a32fcdd67322361e
shubham chamoli <shubhamchamoli8218035988@gmail.com>
2025-01-12 15:26:30 +0530

    Your commit message

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,20 +1,12 @@
 'use client'
-import React , {useEffect , useState} from 'react';
-const login = () =>
 
-{
-    const [message , setMessage] = useState('');
-    useEffect(() =>
-    {
-        fetch('http://localhost:3003/login')
-        .then((response) => response.text())
-        .then((data) => setMessage(data));
+const login = () => {
 
-    },[]);
+
     return (
     <div>
         <h1> this is logi page</h1>
-        <h1>{message}</h1>
+        
     </div>
       
         
